662b812fdfe57b07900722e9ed3803faea378b69
Nathan Rydel <nathanrydel@gmail.com>
2024-01-23 17:36:28 -0700

    updated RouteList to import all required Comps

--- src/RouteList.js
+++ src/RouteList.js
@@ -1,14 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./Homepage";
+import CompanyDetail from "./CompanyDetail";
+import CompanyList from "./CompanyList";
+import JobList from "./JobsList";
 
 
 function RouteList() {
   return (
     <Routes>
       <Route to="/" element={<Homepage />} />
-      <Route to="/companies" element={<CompaniesList />} />
+      <Route to="/companies" element={<CompanyList />} />
       <Route to="/companies/:handle" element={<CompanyDetail />} />
-      <Route to="/jobs" element={<JobsList />} />
+      <Route to="/jobs" element={<JobList />} />
       <Route to="/*" element={<Navigate to="/" />} />
     </Routes>
   );
