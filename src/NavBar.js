4029fa73c4f416b9b5af210615f2600a66edb53b
Nathan Rydel <nathanrydel@gmail.com>
2024-01-24 11:36:28 -0700

    updated JSX rendering for NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar() {
+  console.log("NavBar rendered");
+
   return (
-    <div>NavBar</div>
+    <div className="NavBar">
+      <div className="NavBar-brand">
+        <Link to="/">Jobly</Link>
+      </div>
+      <div className="NavBar-nav">
+        <ul>
+          <li>
+            <NavLink to="/companies">Companies</NavLink>
+          </li>
+          <li>
+            <NavLink to="/jobs">Jobs</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
