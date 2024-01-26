adc81a6924a229e90cb0bbb5d2636d8f60110729
Nathan Rydel <nathanrydel@gmail.com>
2024-01-26 11:24:12 -0700

    added rendering logic to reflect new elements/components

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
+
+import userContext from "./userContext";
 import "./NavBar.css";
 
 /** App NavBar element for navigation
  *
  * Props:
- * - None
+ * - logOut: fn to call from ancestor
  *
  * State:
  * - None
@@ -13,7 +15,8 @@ import "./NavBar.css";
  * App -> NavBar
  */
 
-function NavBar() {
+function NavBar({ logOut }) {
+  const { currUser } = useContext(userContext);
   console.log("NavBar rendered");
 
   return (
@@ -21,15 +24,37 @@ function NavBar() {
       <div className="NavBar-brand">
         <NavLink to="/" end>Jobly</NavLink>
       </div>
+
       <div className="NavBar-nav">
-        <ul>
-          <li>
-            <NavLink to="/companies">Companies</NavLink>
-          </li>
-          <li>
-            <NavLink to="/jobs">Jobs</NavLink>
-          </li>
-        </ul>
+        {currUser &&
+          <ul>
+            <li>
+              <NavLink to="/">Jobly v2</NavLink>
+            </li>
+            <li>
+              <NavLink to="/companies">Companies</NavLink>
+            </li>
+            <li>
+              <NavLink to="/jobs">Jobs</NavLink>
+            </li>
+            <li>
+              <NavLink to="/profile">Profile</NavLink>
+            </li>
+            <li>
+              <a onClick={logOut}>Log out {currUser.userName}</a>
+            </li>
+          </ul>
+        }
+        {!currUser &&
+          <ul>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/signup">Signup</NavLink>
+            </li>
+          </ul>
+        }
       </div>
     </div>
   );
--- src/RouteList.js
+++ src/RouteList.js
@@ -4,28 +4,59 @@ import CompanyDetail from "./CompanyDetail";
 import CompanyList from "./CompanyList";
 import JobList from "./JobList";
 
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+
 /** Site-wide routes
  *
  * Props:
- * - none
+ * - login: fn from parent to login a user
+ * - signup: fn from parent to sign up a user
+ * - currentUser: {}
  *
  * State:
  * - none
  *
  * Visiting a not found route navigates to homepage
  *
+ * Protected Routes:
+ * - CompanyList: /companies
+ * - CompanyDetail: /companies: handle
+ * - JobList: /jobs
+ * - Profile: /profile
+ *
+ * Public Routes:
+ * - Homepage: "/"
+ * - Login: /login
+ * - Signup: /signup
+ *
  * RouteList -> { Homepage, CompanyList, CompanyDetail, JobList }
  */
 
-function RouteList() {
+function RouteList({ login, signUp, currUser }) {
   console.log("RouteList");
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/companies" element={<CompanyList />} />
-        <Route path="/companies/:handle" element={<CompanyDetail />} />
-        <Route path="/jobs" element={<JobList />} />
+        <Route path="/" element={<Homepage user={currUser} />} />
+
+        {!currUser &&
+          <>
+            <Route path="/login" element={<Login login={login} />} />
+            <Route path="/signup" element={<Signup signUp={signUp} />} />
+          </>
+        }
+
+        {currUser &&
+          <>
+            <Route path="/companies" element={<CompanyList />} />
+            <Route path="/companies/:handle" element={<CompanyDetail />} />
+            <Route path="/jobs" element={<JobList />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        }
+
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </div>
