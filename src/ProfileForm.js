238f0494b28738ac6e3e4e128c184417ac461960
Nathan Rydel <nathanrydel@gmail.com>
2024-01-25 17:14:35 -0700

    Part 2.1 initial file commit

---
+++ src/ProfileForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ProfileForm() {
+  return (
+    <div className='ProfileForm'>ProfileForm</div>
+  );
+}
+
+export default ProfileForm;
---
+++ src/SignupForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SignupForm() {
+  return (
+    <div className='SignupForm'>SignupForm</div>
+  );
+}
+
+export default SignupForm;
