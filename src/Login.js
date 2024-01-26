cd88fc291f23c9c4dbae1e0cc8b4149d0856b0b3
Matt Smida <mcs@m4t7.com>
2024-01-25 20:36:35 -0500

    amended login.js to reflect login logic

--- src/Login.js
+++ src/Login.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import LoginForm from './LoginForm';
+
+/**
+ * Renders the login form for the site
+ *
+ * endpoint: /login
+ *
+ * Props:
+ * - login: fn from parent
+ *
+ * State:
+ * - None
+ *
+ * RouteList -> Login -> LoginForm
+ */
 
 function Login() {
   return (
