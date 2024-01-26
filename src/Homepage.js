273cd782e9f2708ade08bacae1a3196efa59ef4b
Matt Smida <mcs@m4t7.com>
2024-01-26 14:31:19 -0500

    added message to homepage based on whether user is logged in

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import userContext from './userContext';
+import { Link } from 'react-router-dom';
 
 /** Renders the homepage of the app
  *
@@ -14,11 +16,21 @@ import React from 'react';
  */
 
 function Homepage() {
+  const { currUser } = useContext(userContext);
+
   console.log("*** Homepage");
   return (
     <div className="Homepage">
       <h1>Jobly</h1>
       <p>All the jobs in one, convenient place.</p>
+      {currUser
+        ? <h2>Welcome back, {currUser.firstName}!</h2>
+        : (
+          <div>
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign up</Link>
+          </div>
+        )}
     </div>
   );
 }
