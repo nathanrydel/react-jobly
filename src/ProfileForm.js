10e09bae805279de978a9e80c6010ad4b461079e
Matt Smida <mcs@m4t7.com>
2024-01-25 20:57:07 -0500

    added profileform

--- src/ProfileForm.js
+++ src/ProfileForm.js
@@ -1,8 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./ProfileForm.css";
 
-function ProfileForm() {
+/**
+ * The profile update form for the site
+ *
+ * Props:
+ *  -updateProfile: fn to call in App
+ *
+ * State:
+ * - formData:
+ * - formErrors:
+ *
+ * Profile -> ProfileForm
+ */
+
+function ProfileForm({ updateProfile }) {
+  const [formData, setFormData] = useState({
+    username: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+  const [formErrors, setFormErrors] = useState([]);
+  const [updateConfirmed, setUpdateConfirmed] = useState(false);
+
+  console.log("ProfileForm renders", formData, formErrors);
+
+  /** Handle user updates on the form by named input field */
+  function handleChange(evt) {
+    const { name, value } = evt.target.value;
+    setFormData((fData) => ({ ...fData, [name]: value }));
+  }
+
+  /** Call function from prop and present confirmation message.
+   *  Set errors if any returned */
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    try {
+      await updateProfile(formData);
+      setUpdateConfirmed(true);
+    } catch (err) {
+      setFormErrors(err);
+    }
+  }
   return (
-    <div className='ProfileForm'>ProfileForm</div>
+    <div className='ProfileForm'>
+      <h2>Profile</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name: </label>
+          <input
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name: </label>
+          <input
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email: </label>
+          <input
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </div>
+
+        {formErrors.length > 0
+          ? <div>{formErrors.map((err, i) => <p key={i}>Error: {formErrors[i]}</p>)}</div>
+          : null
+        }
+
+        {updateConfirmed
+          ? <div><i>Updated successfully.</i></div>
+          : null
+        }
+
+        <div className='ProfileForm-submitBtn'>
+          <button type="submit">Save Changes</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
